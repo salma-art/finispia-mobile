c13067e397e2ba4081fb9a486926f7e821d4cbd3
mohamed <mohamed.kallel@iovision.tn>
2019-07-10 13:51:22 +0100

    integrate new project

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome.component';
+import { StartComponent } from './start/start.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 const routes: Routes = [
   {
     path: '', component: WelcomeComponent, children: [
+      {
+        path: '', component: StartComponent
+      },
       {
         path: 'login', component: LoginComponent
       }
@@ -16,8 +25,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [WelcomeComponent, LoginComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [WelcomeComponent, LoginComponent, StartComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), MatToolbarModule, MatCardModule,MatButtonModule, MatDividerModule],
   exports: [RouterModule]
 })
 export class WelcomeModule { }
